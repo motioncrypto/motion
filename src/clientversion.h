8ad4b264da8c989190ec9d0b34e0ccc91a19a941
Omar Cruz <ocruzvar@gmail.com>
2018-04-21 21:00:35 -0500

    Change Copyright year

--- src/clientversion.h
+++ src/clientversion.h
@@ -25,7 +25,7 @@
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2017
+#define COPYRIGHT_YEAR 2018
 
 #endif //HAVE_CONFIG_H
 
@@ -37,7 +37,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Motion Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Motion Core Developers"
+#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Motion Core Developers"
 
 /**
  * motiond-res.rc includes this file, but it cannot cope with real c++ code.
